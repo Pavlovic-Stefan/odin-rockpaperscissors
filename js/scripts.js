e9a84f7683fbf874a32df3a70276bed7e1abe55a
Stefan <s.pavlovic.rs@gmail.com>
2022-04-01 03:17:36 +0200

    Add everything
    Forgot to commit occasionally

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,26 @@
 let playerScore = 0;
 let computerScore = 0;
 
+const rockButton = document.querySelector('.rock');
+const paperButton = document.querySelector('.paper');
+const scissorsButton = document.querySelector('.scissors');
+
+rockButton.addEventListener('click', () => playRound('rock'));
+paperButton.addEventListener('click', () => playRound('paper'));
+scissorsButton.addEventListener('click', () => playRound('scissors'));
+
+const pcScore = document.querySelector('.pc');
+const cpScore = document.querySelector('.cp');
+
+pcScore.textContent = playerScore;
+cpScore.textContent = computerScore;
+
+const pcChoice = document.querySelector('.pc-choice');
+const cpChoice = document.querySelector('.cp-choice');
+const roundResult = document.querySelector('.round-result');
+
+
+
 function computerPlay() { // Generates a random number between 1 and 3, and turns it into a corresponding rps choice
 
     rpsNumber = Math.floor(Math.random() * 3) + 1
@@ -28,7 +48,38 @@ function rockPaperOrScissors (playerSelection) { // Takes players input, turns i
     }
 }
 
+function rockPaperOrScissorsEmoji (playerSelection) { // Takes players input, turns it all into lowercase, checks what it is and turns it into a corresponding number
+
+    if (playerSelection.toLowerCase()==="rock"){
+        return "&#" + 129704 + ";";
+    } else if (playerSelection.toLowerCase()==="paper"){
+        return "&#" + 128196 + ";";
+    } else if (playerSelection.toLowerCase()==="scissors"){
+        return "&#" + 9996 + ";";
+    } else {
+        return "Invalid selection.";
+    }
+}
+
 function playRound(playerSelection) { // Takes both player and computer choice, substracts computer choice from players and if result is either 1 or -2 player wins, else computer wins
+
+    if (playerScore>=5||computerScore>=5) {
+        playerScore = 0;
+        computerScore = 0;
+        pcScore.textContent = playerScore;
+        cpScore.textContent = computerScore;
+        roundResult.textContent = "";
+        rockButton.classList.remove('hide-buttons');
+        paperButton.classList.remove('paper-font');
+        paperButton.innerHTML = "&#" + 128196 + ";";
+        scissorsButton.classList.remove('hide-buttons');
+        pcChoice.classList.remove('player-choices-winner');
+        cpChoice.classList.remove('player-choices-winner');
+        return;
+    }
+
+    pcChoice.classList.remove('player-choices-winner');
+    cpChoice.classList.remove('player-choices-winner');
 
     let computerSelection = computerPlay()
 
@@ -36,12 +87,39 @@ function playRound(playerSelection) { // Takes both player and computer choice, 
 
     if (result === 1 || result === -2) {
         playerScore++;
-        return "You've won! " + playerSelection.toLowerCase().charAt(0).toUpperCase() + playerSelection.toLowerCase().slice(1) + " beats " + computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1);
+        pcScore.textContent = playerScore;
+        pcChoice.innerHTML = rockPaperOrScissorsEmoji(playerSelection);
+        cpChoice.innerHTML = rockPaperOrScissorsEmoji(computerSelection);
+        pcChoice.classList.add('player-choices-winner');
+        if (playerScore<5) {
+            roundResult.textContent = "You've won! " + playerSelection.toLowerCase().charAt(0).toUpperCase() + playerSelection.toLowerCase().slice(1) + " beats " + computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1);
+        } else {
+            roundResult.textContent = "You've won!"
+            rockButton.classList.add('hide-buttons');
+            paperButton.classList.add('paper-font');
+            paperButton.textContent = 'Play Again?';
+            scissorsButton.classList.add('hide-buttons');
+        }
     } else if (result === 0) {
-        return "It's a draw!";
+        pcChoice.innerHTML = rockPaperOrScissorsEmoji(playerSelection);
+        cpChoice.innerHTML = rockPaperOrScissorsEmoji(computerSelection);
+        roundResult.textContent =  "It's a draw!";
+    
     } else {
         computerScore++;
-        return "You've lost. "  + computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1) + " beats " + playerSelection.toLowerCase().charAt(0).toUpperCase() + playerSelection.toLowerCase().slice(1);
+        cpScore.textContent = computerScore;
+        pcChoice.innerHTML = rockPaperOrScissorsEmoji(playerSelection);
+        cpChoice.innerHTML = rockPaperOrScissorsEmoji(computerSelection);
+        cpChoice.classList.add('player-choices-winner');
+        if (computerScore<5){
+            roundResult.textContent =  "You've lost. "  + computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1) + " beats " + playerSelection.toLowerCase().charAt(0).toUpperCase() + playerSelection.toLowerCase().slice(1);
+        } else {
+            roundResult.textContent =  "You've lost."
+            rockButton.classList.add('hide-buttons');
+            paperButton.classList.add('paper-font');
+            paperButton.textContent = 'Play Again?';
+            scissorsButton.classList.add('hide-buttons');
+        }
     }
 
 }
